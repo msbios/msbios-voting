a5d6364cb8f3a6c36bd791693f942dffd3061aeb
Judzhin Miles <info@msbios.com>
2017-10-24 11:10:02 +0300

    Lazy Composer Commiter

--- config/module.config.php
+++ config/module.config.php
@@ -31,7 +31,7 @@ return [
     'form_elements' => [
         'factories' => [
             PollForm::class =>
-                InvokableFactory::class
+                Factory\PollFormFactory::class
         ],
         'aliases' => [
             View\Helper\PollHelper::class =>
@@ -53,38 +53,60 @@ return [
     ],
 
     Module::class => [
-        'polls' => [
-            'SOME_IDENTIFIER_KEY' => [
-                'subject' => 'Rating of trust',
-                'total' => 48,
-                'options' => [
-                    [
-                        'id' => 1,
-                        'name' => 'I totally trust',
-                        'total' => 200,
-                        'percent' => '20%',
-                        'priority' => 1
-                    ], [
-                        'id' => 2,
-                        'name' => 'I\'m more likely to trust',
-                        'total' => 200,
-                        'percent' => '20%',
-                        'priority' => 0
-                    ], [
-                        'id' => 3,
-                        'name' => 'Most likely I do not trust',
-                        'total' => 200,
-                        'percent' => '20%',
-                        'priority' => 2
-                    ], [
-                        'id' => 4,
-                        'name' => 'I totally do not trust',
-                        'total' => 200,
-                        'percent' => '20%',
-                        'priority' => 1
-                    ],
-                ]
-            ]
-        ]
+
+        /**
+         *
+         * Expects: string
+         * Default: poll_option_identifier
+         */
+        'default_option_key' => 'poll_option_identifier',
+
+        /**
+         *
+         * Expects: string
+         * Default: poll_relation
+         */
+        'default_relation_key' => 'poll_relation',
+
+        /**
+         *
+         * Expects: string
+         * Default: poll_handler
+         */
+        'default_handler_key' => 'poll_identifier',
+
+        //'polls' => [
+        //    'SOME_IDENTIFIER_KEY' => [
+        //        'subject' => 'Rating of trust',
+        //        'total' => 48,
+        //        'options' => [
+        //            [
+        //                'id' => 1,
+        //                'name' => 'I totally trust',
+        //                'total' => 200,
+        //                'percent' => '20%',
+        //                'priority' => 1
+        //            ], [
+        //                'id' => 2,
+        //                'name' => 'I\'m more likely to trust',
+        //                'total' => 200,
+        //                'percent' => '20%',
+        //                'priority' => 0
+        //            ], [
+        //                'id' => 3,
+        //                'name' => 'Most likely I do not trust',
+        //                'total' => 200,
+        //                'percent' => '20%',
+        //                'priority' => 2
+        //            ], [
+        //                'id' => 4,
+        //                'name' => 'I totally do not trust',
+        //                'total' => 200,
+        //                'percent' => '20%',
+        //                'priority' => 1
+        //            ],
+        //        ]
+        //    ]
+        //]
     ]
 ];
--- src/PollForm.php
+++ src/PollForm.php
@@ -5,8 +5,10 @@
  */
 namespace MSBios\Voting;
 
+use Zend\Config\Config;
 use Zend\Form\Element\Hidden;
 use Zend\Form\Element\Radio;
+use Zend\Form\Element\Submit;
 use Zend\Form\Form;
 
 /**
@@ -15,18 +17,33 @@ use Zend\Form\Form;
  */
 class PollForm extends Form
 {
+    /** @var Config  */
+    protected $config;
+
+    /**
+     * PollForm constructor.
+     * @param Config $config
+     * @param null $name
+     * @param array $options
+     */
+    public function __construct(Config $config, $name = null, array $options = [])
+    {
+        $this->config = $config;
+        parent::__construct($name, $options);
+    }
+
     public function init()
     {
         parent::init();
         $this->add([
             'type' => Radio::class,
-            'name' => 'poll_option_identifier'
+            'name' => $this->config->get('default_option_key')
         ])->add([
             'type' => Hidden::class,
-            'name' => 'poll_identifier'
+            'name' => $this->config->get('default_relation_key')
         ])->add([
-            'type' => Hidden::class,
-            'name' => 'poll_relation'
+            'type' => Submit::class,
+            'name' => $this->config->get('default_handler_key')
         ]);
     }
 }
