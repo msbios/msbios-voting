3475e5015c0f5de62019560a77571c09b49c042f
Judzhin Miles <info@msbios.com>
2018-02-08 13:01:07 +0200

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -19,7 +19,7 @@ class Module implements
     AutoloaderProviderInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.34';
+    const VERSION = '1.0.35';
 
     /**
      * Returns configuration to merge with application configuration
